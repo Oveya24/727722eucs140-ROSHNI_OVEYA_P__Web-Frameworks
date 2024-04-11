5c15d884c0c0aed0cb2434872f5bc7a1725fb250
Oveya24 <124981194+Oveya24@users.noreply.github.com>
2024-04-12 00:01:56 +0530

    Add files via upload

---
+++ Day 1/Day 2/demo2/src/main/java/com/example/demo2/model/Student.java
@@ -0,0 +1,27 @@
+package com.example.demo2.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Student
+{
+    @JsonProperty("Student created")
+    String name;
+    @JsonProperty("Age")
+    int age;
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
